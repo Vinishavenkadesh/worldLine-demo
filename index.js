15993f9c9b2b94b29090b0f2cd49478c3296438b
Vinishavenkadesh <vinishabindhu03@gmail.com>
2023-04-05 23:15:57 +0530

    commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors")
+const mysql = require("mysql2")
+const path = require("path")
+const app = express()
+
+const PORT = 3000;
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname + "/static")))
+
+const sql = mysql.createPool({
+    host : "localhost",
+    username : "root",
+    password : "password",
+    database : "forms",
+}).promise();
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/static/index.html")
+})
+
+app.get("/get-Posts",async(req,res)=>{
+    const query = "select * from forms";
+    const [output] = await sql.query(query);
+    res.send(output)
+})
+
+app.post("/postData", async(req,res) => {
+    const phoneNumber = req.body.phoneNumber;
+    const email = req.body.email;
+    const query = "insert into forms(phoneNumber,email) values(?,?)";
+    await sql.query(query,[phoneNumber,email]);
+    console.log("POSTED SUCCESSFULLY");
+
+})
+
+
+app.listen(PORT, ()=> {
+    console.log(`Listening to the PORT : ` + PORT);
+})
